6175d0440a918cc4a918a852a314bcd6bc0e4a74
Sarvesh kumar <saursarv@gmail.com>
2023-12-04 20:13:10 +0530

    pattern programming started.

--- Pattern-programming/pattern2.cpp
+++ Pattern-programming/pattern2.cpp
@@ -1,12 +1,11 @@
 /*
-Pattern1:
-=========
 1 2 3 4 5 
-1 2 3 4 5 
-1 2 3 4 5 
-1 2 3 4 5 
-1 2 3 4 5 
+2 3 4 5 6 
+3 4 5 6 7 
+4 5 6 7 8 
+5 6 7 8 9 
 */
+
 
 #include <iostream>
 using namespace std;
@@ -14,10 +13,12 @@ int main() {
   int n=5;
   int row=1;
   while(row<=n){
+    int count=row;
     int col=1;
     while(col<=n){
-      cout<< col <<" ";
+      cout<< count <<" ";
       col++;
+      count++;    
     }
     cout << endl;
     row=row+1;
